1e7784cb132b7b7fa13c7dea5f69557d548451c8
Carlos Gonzalez <carlos_gonzalez82201@elpoli.edu.co>
2025-11-09 15:54:01 -0500

    add userrole enum, add adminmiddleware and set bootstrap alias, ratelimiter for api calls

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check() || auth()->user()->role !== UserRole::ADMIN) {
+            return response()->json(['message' => 'Acceso denegado. Requiere rol de administrador.'], 403);
+        }
+        return $next($request);
+    }
+}
