c318329dd30e9853bbcb65dd34df2debc90642b8
Carlos Gonzalez <carlos_gonzalez82201@elpoli.edu.co>
2025-11-09 16:07:56 -0500

    add feat in users migration for unique_library_id and role, new authors books and book_user's tables w/models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('unique_library_id')->unique();
+            $table->string('role')->default('User');
             $table->rememberToken();
             $table->timestamps();
         });
